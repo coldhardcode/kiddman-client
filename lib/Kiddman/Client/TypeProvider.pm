aba455c94da42e8d6e2f4d409ea1a1bfb0a17e3c
gphat <gphat@cory-watsons-macpro31.local>
2009-04-01 10:23:05 -0500

    Add error accessor to provider

--- lib/Kiddman/Client/TypeProvider.pm
+++ lib/Kiddman/Client/TypeProvider.pm
@@ -1,6 +1,11 @@
 package Kiddman::Client::TypeProvider;
 use Moose::Role;
 
+
+has 'error_string' => (
+    is => 'rw',
+    isa => 'Str'
+);
 has 'value_accessor' => (
     is => 'ro',
     isa => 'Maybe[Str]',
